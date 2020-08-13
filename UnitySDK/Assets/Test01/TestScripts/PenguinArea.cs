6c9b2d864f3c5732729f7283be042fffdb6b504c
egoquat <egoquat@users.noreply.github.com>
2020-08-13 12:12:51 +0900

    Start

---
+++ UnitySDK/Assets/Test01/TestScripts/PenguinArea.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+using TMPro;
+
+public class PenguinArea : MonoBehaviour 
+{
+    public PenguinAgent penguinAgent;
+    public GameObject penguinBaby;
+    public PenguinFish prefabFish;
+    public TextMeshPro cumulativeRewardText;
+
+
+}
